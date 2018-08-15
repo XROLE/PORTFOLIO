66509ba93ce0f7c4846ab3e7f8997db470d854d2
XROLE <xrolediamond@gmail.com>
2018-08-14 21:31:19 -0700

    configured the server to run on port 3000

--- server/app.js
+++ server/app.js
@@ -1,1 +1,14 @@
--
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+    res.send('I am a chosen one');
+})
+
+app.listen(port, function(){
+    console.log('server started on port 3000')
+});
