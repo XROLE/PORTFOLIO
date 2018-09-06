4e0c5ab34a18572926c1b29aba81b974ddd2cbca
XROLE <xrolediamond@gmail.com>
2018-09-06 16:00:35 -0700

    changed port to fix remote error

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 app.use('/', router);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, function(){
     console.log('server started on port ' + port)
